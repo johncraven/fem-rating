abb81172e2806b253318ad48494c492a4bd5d9c1
John Craven <johndcraven@gmail.com>
2023-08-31 14:19:36 -0400

    added click event listener for the submit button

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const ratingCard = document.querySelector("#rating-card");
 const submitButton = document.querySelector("button");
 const thanksCard = document.querySelector("#thanks-card");
 
-
+submitButton.addEventListener("click", () => {
+    ratingCard.classList.add('hide');
+    thanksCard.classList.remove('hide')
+})
 // ratingCard.classList.add('hide')
 // thanksCard.classList.remove('hide')
