b6f915667feb06490c0417f543f45f68d7267f2c
John Craven <johndcraven@gmail.com>
2023-08-31 14:14:00 -0400

    initializing...

---
+++ main.js
@@ -0,0 +1,7 @@
+const ratingCard = document.querySelector("#rating-card");
+const submitButton = document.querySelector("button");
+const thanksCard = document.querySelector("#thanks-card");
+
+
+// ratingCard.classList.add('hide')
+// thanksCard.classList.remove('hide')
